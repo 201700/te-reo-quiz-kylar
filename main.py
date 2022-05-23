4cc013341bdf3378d695c128b51375e1333c8fd6
Kylar Mancer <105251207+201700@users.noreply.github.com>
2022-05-23 22:46:47 +0000

    rewrite question one

---
+++ main.py
@@ -0,0 +1,14 @@
+def question1():
+ print("this is question one")
+ 
+Choice = input("Mōrena means good morning in te reo / true or false? ")
+Choice.lower
+  
+if Choice == "true":
+  print("correct")
+  question2()
+elif Choice == "false":
+  print("incorrect")
+  question2()
+
+
