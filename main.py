3641e578479851e11892d9436ab16cc3112c82da
Kylar Mancer <105251207+201700@users.noreply.github.com>
2022-05-26 01:36:41 +0000

    added comments

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-name=input 
-print()
+#asking what your names is, greeting you with your name,  and explaing that this is a te reo quiz and explaing what you'll do while doing it.
+name=input("what is your name?")
+print("hello", name)
+print("this is a te reo quiz, doing this quiz you will know or learn te reo phases")
 
+#this is question one, in questions one it ask you a question if Mōrena the means good morning in english. this can be correct or False. if you enter true or false, then it will tell you if you are correct or incorrect. then it will move you to question 2.
 def question1():
-  print("this is question one")
-  choice = input("Mōrena means good morning in te reo / true or false? ")
-  choice = choice.lower()
+ print("this is question one")
+ choice = input("Mōrena means good morning in te reo / true or false? ")
+ choice = choice.lower()
   
-  if choice == "true":
+ if choice == "true":
     print("correct")
     question2()
-  elif choice == "false":
+ elif choice == "false":
     print("incorrect")
     question2()
-  
+
+#this is question two, in questions two it ask you a question if purū  the means green in english. this can be correct or False. if you enter true or false,then it will tell you if you are correct or incorrect. then it will move you to question 3.
 def question2():
   print("this is question two")
-  space = input("kākāriki means green in te reo / true or false? ")
+  space = input("purū means green in te reo / true or false? ")
   space = space.lower()
   if space == "true":
-   print("correct")
+   print("incorrect")
    question3()
   elif space == "false":
-    print("incorrect")
+    print("correct")
     question3()
-
+#this is question three, in questions three it ask you a question if Kei te nga mihi  the means  how are you in english. this can be correct or False. if you enter true or false,then it will tell you if you are correct or incorrect. then it will move you to question 4.
 def question3():
   print("this is question three")
-  color = input("Kei te pēhea koe means how are you in te reo / true or false? ")
+  color = input("Kei te nga mihi means how are you in te reo / true or false? ")
   color = color.lower()
   
   if color == "true":
@@ -35,7 +39,7 @@ def question3():
   elif color == "false":
     print("incorrect")
     question4()
-
+#this is question two, in questions four it ask you a question if akomanga the means classroom in english. this can be correct or False. if you enter true or false,then it will tell you if you are correct or incorrect. then it will move you to question 5.
 def question4():
   print("this is question four")
   size = input("akomanga means classroom in te reo / true or false? ")
@@ -46,7 +50,7 @@ def question4():
   elif size == "false":
    print("incorrect")
    question5()
-
+#this is question five, in questions five it ask you a question if rorohiko the means computer in english. this can be correct or False. if you enter true or false,then it will tell you if you are correct or incorrect. then it will end.
 def question5():
   print("this is question five")
   line = input("rorohiko means computer in te reo / true or false? ")
